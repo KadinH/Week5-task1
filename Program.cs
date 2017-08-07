f647122a04fd7027f00ada36e9ccad994a214d87
Kadin Honeyfield <9983505@BCS.NET.NZ>
2017-08-08 11:48:53 +1200

    commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Week5_task1
 {
@@ -7,6 +8,49 @@ namespace Week5_task1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            List<Shape> Shapes = new List<Shape>();
+            Shapes.Add(new Rectangle());
+            Shapes.Add(new Circle());
+            Shapes.Add(new Triangle());
+
+        foreach (Shape x in Shapes)
+        {
+            x.Draw();
         }
+        }
+    }
+    public class Shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Virtual Draw method");
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("This is a Rectangle");
+            base.Draw();
+        }
+    }
+    class Circle : Shape
+    {
+        public override void Draw() 
+        {
+            Console.WriteLine("This is a Circle");
+            base.Draw();
+        }
+
+    }
+    class Triangle : Shape
+    {
+        public override void Draw() 
+        {
+            Console.WriteLine("This is a Triangle");
+            base.Draw();
+        }
+
     }
 }
