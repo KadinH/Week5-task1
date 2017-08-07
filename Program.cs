29c50fd3a7cdc563331c1661c01108b4c048a489
Kadin Honeyfield <9983505@BCS.NET.NZ>
2017-08-08 11:05:02 +1200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week5_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
